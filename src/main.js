ea0834475fec8c5030decb20596e4efe601b62a4
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2018-11-22 08:45:17 +0800

    vub

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const vub = require('viewport-units-buggyfill')
+
+window.addEventListener('load', () => {
+  vub.init({ hacks: window.viewportUnitsBuggyfillHacks })
+})
+
 Vue.config.productionTip = false
 
 new Vue({
